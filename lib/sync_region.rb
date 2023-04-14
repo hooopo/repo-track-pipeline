a49ba8bac9ab86ac48efcbd2263ee1e1bf75b784
Hooopo <Hoooopo@gmail.com>
2023-04-14 11:08:36 +0800

    Update sync_region.rb

--- lib/sync_region.rb
+++ lib/sync_region.rb
@@ -1,7 +1,7 @@
 class SyncRegion 
   def run
     User.where("location is not null and location <> '' and region is null").limit(5000).each do |user|
-      region = Geocoder.search(user.location)&.first&.country
+      region = Geocoder.search(user.location)&.first&.country rescue nil
       if region.present?
         User.where(id: user.id).update_all(region: region) 
       else
