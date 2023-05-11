5d4a9c9042eeb003b66b808994a6b413f0f3a03e
Hooopo Wang <hoooopo@gmail.com>
2023-05-11 14:30:02 +0800

    Add interval for failed migration

--- config/initializers/retryable_rake.rb
+++ config/initializers/retryable_rake.rb
@@ -7,6 +7,7 @@ class RetryableRake
   def self.db_migrate 
     Retryable.retryable(tries: 10, matching: /Lost connection to MySQL server/) do |retries, exception|
       puts "Retrying #{retries} times due to #{exception}" if retries > 0
+      sleep(rand(30 * retries)) if retries > 0
       Rake::Task['db:migrate'].invoke
     end
   end
@@ -14,6 +15,7 @@ class RetryableRake
   def self.db_create 
     Retryable.retryable(tries: 10, matching: /Lost connection to MySQL server/) do |retries, exception|
       puts "Retrying #{retries} times due to #{exception}" if retries > 0
+      sleep(rand(30 * retries)) if retries > 0
       Rake::Task['db:create'].invoke
     end
   end
